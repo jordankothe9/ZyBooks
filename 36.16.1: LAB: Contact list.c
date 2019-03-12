ddd94cf3db6416c6db9f2597ed4499c23c2b2024
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-12 14:50:09 -0400

    Create 36.16.1: LAB: Contact list.c
    main complete, search function needs to be finished

---
+++ 36.16.1: LAB: Contact list.c
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//search function
+string GetPhoneNumber(vector<string> nameVec, vector<string> phoneNumberVec, string contactName){
+   //todo
+}
+
+int main() {
+   //initalize vars
+   vector<string> nameVec;
+   vector<string> phoneNumberVec;
+   int length
+   string contactName;
+   
+   //get user data starting with number of entries followed by name, phone, name, phone etc
+   scanf("%d", &length);
+   for(int i = 0; i < length; i++){
+      scanf("%s %s", &nameVec.at(i), &phoneNumberVec.at(i);
+   }
+   
+   //call search function and print phone number
+   scanf("%s", contactName);
+   printf(GetPhoneNumber(contactName));
+
+   return 0;
+}
