472fe5925dbd20d84abde815f3bfcbe582cacff9
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-13 13:25:23 -0400

    Added GetFrequencyOfWord function

--- 36.20 LAB: Word frequencies - functions.cpp
+++ 36.20 LAB: Word frequencies - functions.cpp
@@ -3,7 +3,21 @@
 #include <string>
 using namespace std;
 
-/* Define your function here */
+int GetFrequencyOfWord(vector<string> wordsList, string currWord){
+   int size = vector.size();
+   //keep track of occurances of word
+   int count = 0;
+   
+   //search through vector for matches adding to count for each match
+   for(int i = 0; i < size; i++){
+      if(currWord == vector.at(i))
+         count++;
+   }
+   
+   return count;
+}
+
+
 
 int main() {
    /* Type your code here */
