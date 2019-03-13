9d26be41ed711d26514cc32d6830acb4906a0af2
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-13 13:51:31 -0400

    Rough program with bugs

--- 42.4.1: LAB: Driving cost - functions.c
+++ 42.4.1: LAB: Driving cost - functions.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
-/* Define your function here */ 
+//Driving cost: miles * (gals/miles) * ( $ / gals ) = dollars
+
+//cost per mile ($ /miles) = gals/mile.........
+
+double DrivingCost(double drivenMiles, double mpg, double dollarsPerGallon){
+   return miles * mpg * dollarsPerGallon;
+}
 
 int main(void) {
 
-   /* Type your code here. */
+
+   //input: car's miles/gallon and  dollars/gallon (both doubles)
+   double mpg;
+   double dollarsPerGallon;
+   
+   cin >> mpg;
+   cin >> dollarsPerGallon;
+   
+   //outputs should include: 10 miles, 50 miles, and 400 miles
+   cout << DrivingCost(10.0, mpg, dollarsPerGallon);
+   cout << DrivingCost(50.0, mpg, dollarsPerGallon);
+   cout << DrivingCost(400.0, mpg, dollarsPerGallon);
 
    return 0;
 }
