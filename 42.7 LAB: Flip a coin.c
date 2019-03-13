1f1caae506c091fe6921f24af337b88d308d3c6f
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-13 15:04:01 -0400

    ...after fucking around for an hour no luck

--- 42.7 LAB: Flip a coin.c
+++ 42.7 LAB: Flip a coin.c
@@ -4,28 +4,25 @@
 
 void HeadsOrTails(char* decisionString){
    //rand() % (max_number + 1 - minimum_number) + minimum_number
-   //restrict output to 1 or 2
-   int decisionNum = rand() % (2 + 1 - 1) + 1;
+   double decisionNum = rand() % (2 + 1 - 1) + 1;
    
-   //change num to string
-   if(num = 1)
-      decisionString = "heads";
+   //printf("%lf", decisionNum);
+   
+   if(decisionNum == 1)
+      *decisionString = "heads";
    else
-      decisionString = "tails";
+      *decisionString = "tails";
 }
 
 int main(void) {
-   //declair
    int numRuns;
-   char decisionString;
-   
-   //userinput
+   char decisionString[5] = "tests";
+   printf("%s\n", decisionString);
    scanf("%d", &numRuns);
    srand(2);  /* Unique seed */
    
-   //flip the coin
    HeadsOrTails(&decisionString);
-   printf("%c\n", decisionString);
+   printf("%s\n", decisionString);
    
    
    return 0;
