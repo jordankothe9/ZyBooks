f15e11e4f5e00145bb4fa80a25a5e10b94292507
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-13 13:54:27 -0400

    bug fixes
    Fixed miles in function
    switched to scanf because it's C (duh)
    switched to printf

--- 42.4.1: LAB: Driving cost - functions.c
+++ 42.4.1: LAB: Driving cost - functions.c
@@ -5,7 +5,7 @@
 //cost per mile ($ /miles) = gals/mile.........
 
 double DrivingCost(double drivenMiles, double mpg, double dollarsPerGallon){
-   return miles * mpg * dollarsPerGallon;
+   return drivenMiles * mpg * dollarsPerGallon;
 }
 
 int main(void) {
@@ -15,13 +15,14 @@ int main(void) {
    double mpg;
    double dollarsPerGallon;
    
-   cin >> mpg;
-   cin >> dollarsPerGallon;
+   scanf("%lf", &mpg);
+   scanf("%lf", &dollarsPerGallon);
    
    //outputs should include: 10 miles, 50 miles, and 400 miles
-   cout << DrivingCost(10.0, mpg, dollarsPerGallon);
-   cout << DrivingCost(50.0, mpg, dollarsPerGallon);
-   cout << DrivingCost(400.0, mpg, dollarsPerGallon);
+   
+   printf(DrivingCost(10.0, mpg, dollarsPerGallon));
+   printf(DrivingCost(50.0, mpg, dollarsPerGallon));
+   printf(DrivingCost(400.0, mpg, dollarsPerGallon));
 
    return 0;
 }
