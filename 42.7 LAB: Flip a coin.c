2865fa5019a93c0c10e56061d90156311fb64701
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-13 14:14:05 -0400

    Create 42.7 LAB: Flip a coin.c
    Write a program that simulates flipping a coin to make decisions. The input is how many decisions are needed, and the output is either heads or tails. Assume the input is a value greater than 0.
    
    Ex: If the input is 3, the output is:
    
    heads
    tails
    heads
    For reproducibility needed for auto-grading, seed the program with a value of 2. In a real program, you would seed with the current time. In that case, every program's output would be different, which is what is desired but can't be auto-graded.
    
    Note: A common student mistake is to call srand() before each call to rand(). But seeding should only be done once, at the start of the program, after which rand() can be called any number of times.
    
    Your program must define and call the following function that assigns decisionString with "heads" or "tails".
    void HeadsOrTails(char* decisionString)

---
+++ 42.7 LAB: Flip a coin.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Define your function here */ 
+
+int main(void) {
+   /* Add more variables as needed */
+
+   srand(2);  /* Unique seed */
+
+   /* Type your code here. */
+   
+   return 0;
+}
