5affb06f995e41533a59d82f7d9bee01908d1577
jordankothe9 <48492460+jordankothe9@users.noreply.github.com>
2019-03-21 18:47:56 -0400

    Create 22.6.1: Pointers for allocating a C string.c
    Use strlen(userStr) to allocate exactly enough memory for newStr to hold the string in userStr (Hint: do NOT just allocate a size of 100 chars).

---
+++ Chapter22/22.6.1: Pointers for allocating a C string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+   char userStr[100];
+   char* newStr = NULL;
+
+   strcpy(userStr, "Hello friend!");
+
+   /* Your solution goes here  */
+
+   strcpy(newStr, userStr);
+   printf("%s\n", newStr);
+
+   return 0;
+}
